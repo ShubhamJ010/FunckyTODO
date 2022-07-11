e7656edbd176a8d2e4af2c399fad88f99eb44ea8
udit <uditeeiot@gmailcom>
2022-07-11 21:01:21 +0530

    Updates

---
+++ precache-manifest.bd3287c98206d67866c724532318b8b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10345a54a5f9f3dba18cade84dafc570",
+    "url": "/FunkyTODO/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/FunkyTODO/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "353ebbef78938948efe4",
+    "url": "/FunkyTODO/static/js/main.cca28e9c.chunk.js"
+  },
+  {
+    "revision": "696da1e1dabb2b5e00f3",
+    "url": "/FunkyTODO/static/js/runtime-main.e07a938e.js"
+  }
+]);
